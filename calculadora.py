d1419727f9ea563c1e7d0a5a9ad4b1d6e77a7627
DanielaGit16 <danielamedinavalbuena@gmail.com>
2025-11-30 23:04:55 -0500

    Agregar un tercer numero a la operacion

--- calculadora.py
+++ calculadora.py
@@ -2,17 +2,18 @@
 # Script básico para operaciones matemáticas
 numero_1 = float(input("Primer número: "))
 numero_2 = float(input("Segundo número: "))
+numero_3 = float(input("Tercer número: "))
 operacion = input("Operación (+, -, *, /): ")
 
 if operacion == '+':
-    print("Resultado:", numero_1 + numero_2)
+    print("Resultado:", numero_1 + numero_2 + numero_3)
 elif operacion == '-':
-    print("Resultado:", numero_1 - numero_2)
+    print("Resultado:", numero_1 - numero_2 - numero_3)
 elif operacion == '*':
-    print("Resultado:", numero_1 * numero_2)
+    print("Resultado:", numero_1 * numero_2 * numero_3)
 elif operacion == '/':
-    if numero_2 != 0:
-        print("Resultado:", numero_1 / numero_2)
+    if numero_3 != 0:
+        print("Resultado:", numero_1 / numero_2 / numero_3)
     else:
         print("Error: no se puede dividir por cero.")
         
